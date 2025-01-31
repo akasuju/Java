343ef95d491771fb7ac5e7056bd93229420f8ffc
Sujan khatri <sujan.chettri1321@gmail.com>
2025-01-31 07:34:11 +0545

    Enhance GridBagLayoutTest with additional button arrangements and layout configurations

--- GridBagLayoutTest.java
+++ GridBagLayoutTest.java
@@ -1,36 +1,70 @@
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class GridBagLayoutTest {
-      public static void main (String [] args) {
-        JFrame jf = new JFrame("This is ");
+      public static void main(String[] args) {
+        JFrame jf = new JFrame();
         jf.setSize(300,300);
-        jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
-        GridBagLayout gbl = new GridBagLayout();
-        jf.setLayout(gbl);
+        
+        
+        GridBagLayout gBL= new GridBagLayout();
+        jf.setLayout(gBL);
         GridBagConstraints gbc = new GridBagConstraints();
-      
-        JButton b1 =new JButton("one");
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbl.setConstraints(b1, gbc);
+        
+
+
+
+        
+//        buttons defined       
+        JButton b1 = new JButton ("1");
+        gbc.gridx=0;
+        gbc.gridy=0;
+        gBL.setConstraints(b1, gbc);
         jf.add(b1);
-        JButton b2 =new JButton("two");
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbl.setConstraints(b2, gbc);
+          
+        JButton b2 = new JButton ("2");
+        gbc.gridx=1;
+        gbc.gridy=0;
+        gBL.setConstraints(b2, gbc);
         jf.add(b2);
-        JButton b3 =new JButton("three");
-        JButton b4 =new JButton("four");
-     
-        jf.add(b1);
-        jf.add(b2);
-        jf.add(b3);
+        
+        
+        JButton b4 = new JButton ("4");
+        gbc.gridx=1;
+        gbc.gridy=1;
+        gBL.setConstraints(b4, gbc);
         jf.add(b4);
+        
+         JButton b5 = new JButton ("5");
+        gbc.gridx=1;
+        gbc.gridy=2;
+        gBL.setConstraints(b5, gbc);
+        jf.add(b5);
+        
+         JButton b6 = new JButton ("6");
+        gbc.gridx=0;
+        gbc.gridy=3;
+        gbc.gridwidth=2;
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gBL.setConstraints(b6, gbc);
+        jf.add(b6);
+        
+        JButton b3 = new JButton ("3");
+        gbc.gridx=0;
+        gbc.gridy=1;
+        gbc.gridheight=2;
+        gbc.gridwidth=1;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        gBL.setConstraints(b3, gbc);
+        jf.add(b3);
+        
+      
+
+
+//setting visibility
+      jf.setVisible(true);  
     }
+
+    
 }
