5a4fbda6fafbf09a647db90bd285acf216353c0a
Sujan khatri <sujan.chettri1321@gmail.com>
2025-02-06 07:15:46 +0545

    Add popupbuttontest class with button click functionality and basic JFrame setup

---
+++ popupbuttontest.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class popupbuttontest implements ActionListener {
+    JFrame jf;
+    JButton b1;
+
+    popupbuttontest() {
+        JFrame jf = new JFrame();
+        jf.setSize(300, 300);
+        jf.setLayout(new FlowLayout());
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton b1 = new JButton("click me");
+        jf.add(b1);
+        b1.addActionListener(this);
+        jf.setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "You clicked the button");
+    }
+}
